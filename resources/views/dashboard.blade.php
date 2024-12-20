f4c084f00bdab1b42280919464dad11e0535c138
hera@gmail.com <hera@gmail.com>
2024-12-20 15:31:14 +0700

    pert 20

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,21 @@
 @extends('template')
 @section('judul', 'Dashboard')
 @section('isi')
+  <section id="navigasi">
+    <ul>
+      <li><a href="">Home</a> </li>
+      <li><a href="">Jabatan</a> </li>
+      <li><a href="">Karyawan</a></li>
+    </ul>
+  </section>
   <div id="hakim">
-    <div id="zakiah">ini konten pertama</div>
-    <div id="adinda">ini konten kedua</div>
+    <div id="zakiah">
+      <h2 class="judul">Absensi Karyawan</h2>
+      <p class="sub-judul">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquid dolore aliquam necessitatibus tempora quibusdam porro non fugiat neque ratione, minus, nihil adipisci harum nisi inventore tempore sapiente illo debitis repellendus ipsum corporis officiis vitae reiciendis? Mollitia magnam nulla repellat, sequi sunt fuga numquam distinctio possimus dignissimos eius debitis dolore.</p>
+
+    </div>
+    <div id="adinda">
+      <img width="400" src="{{ asset('gambar.svg') }}" alt="">
+    </div>
   </div>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,17 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('judul')</title>
     <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        #navigasi ul {
+            background-color: slategray;
+            margin: 0;
+            list-style-type: none;
+            overflow: hidden;
+        }
+
+        #navigasi ul li {
+            float: left;
+        }
+
+        #navigasi ul li a {
+            display: block;
+            text-align:center;
+            padding: 14px 16px;
+            text-decoration: none;
+            color: white;
+        }
         #hakim {
-            background-color: greenyellow;
             height: 200px;
             display: grid;
-            grid-template-columns: auto auto;
+            grid-template-columns: 40% 40%;
+            justify-content: center;
         }
         #zakiah {
-            background-color: pink;
+          
         }
         #adinda {
-            background-color: plum;
+           
+        }
+
+        .judul {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 64px;
+            margin-bottom: 0;
+        }
+
+        .sub-judul {
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-size: 18px;
         }
     </style>
 </head>
